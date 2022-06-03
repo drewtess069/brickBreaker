f678322190ea2f89abd13a486851d69af1d590c0
drewtess069 <drewtess069@ed.amdsb.ca>
2022-06-03 13:51:28 -0400

    Add project files.

---
+++ brickBreaker/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace brickBreaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.heart3 = new System.Windows.Forms.PictureBox();
+            this.heart2 = new System.Windows.Forms.PictureBox();
+            this.heart1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.heart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Arcade Interlaced", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, 151);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(500, 50);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "BRICK BREAKER";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTitleLabel.Font = new System.Drawing.Font("Arcade Interlaced", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.Location = new System.Drawing.Point(13, 211);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(473, 128);
+            this.subTitleLabel.TabIndex = 1;
+            this.subTitleLabel.Text = "Press Space to Begin";
+            this.subTitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Arcade Interlaced", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(12, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(100, 23);
+            this.timeLabel.TabIndex = 2;
+            this.timeLabel.Text = "time";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // continueButton
+            // 
+            this.continueButton.Enabled = false;
+            this.continueButton.Font = new System.Drawing.Font("Arcade Interlaced", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.continueButton.Location = new System.Drawing.Point(261, 333);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(81, 43);
+            this.continueButton.TabIndex = 4;
+            this.continueButton.Text = "NO";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Visible = false;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Enabled = false;
+            this.closeButton.Font = new System.Drawing.Font("Arcade Interlaced", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(162, 333);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(88, 43);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "YES";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Visible = false;
+            // 
+            // heart3
+            // 
+            this.heart3.BackColor = System.Drawing.Color.Transparent;
+            this.heart3.BackgroundImage = global::brickBreaker.Properties.Resources.heart;
+            this.heart3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.heart3.Location = new System.Drawing.Point(461, 14);
+            this.heart3.Name = "heart3";
+            this.heart3.Size = new System.Drawing.Size(22, 23);
+            this.heart3.TabIndex = 7;
+            this.heart3.TabStop = false;
+            // 
+            // heart2
+            // 
+            this.heart2.BackColor = System.Drawing.Color.Transparent;
+            this.heart2.BackgroundImage = global::brickBreaker.Properties.Resources.heart;
+            this.heart2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.heart2.Location = new System.Drawing.Point(433, 14);
+            this.heart2.Name = "heart2";
+            this.heart2.Size = new System.Drawing.Size(22, 23);
+            this.heart2.TabIndex = 8;
+            this.heart2.TabStop = false;
+            // 
+            // heart1
+            // 
+            this.heart1.BackColor = System.Drawing.Color.Transparent;
+            this.heart1.BackgroundImage = global::brickBreaker.Properties.Resources.heart;
+            this.heart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.heart1.Location = new System.Drawing.Point(405, 14);
+            this.heart1.Name = "heart1";
+            this.heart1.Size = new System.Drawing.Size(22, 23);
+            this.heart1.TabIndex = 9;
+            this.heart1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(500, 600);
+            this.Controls.Add(this.heart1);
+            this.Controls.Add(this.heart2);
+            this.Controls.Add(this.heart3);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Brick Breaker";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.heart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button continueButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.PictureBox heart3;
+        private System.Windows.Forms.PictureBox heart2;
+        private System.Windows.Forms.PictureBox heart1;
+    }
+}
+
---
+++ brickBreaker/Form1.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace brickBreaker
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player = new Rectangle(220, 550, 60, 10);
+        Rectangle border = new Rectangle(0, 0, 500, 600);
+        Rectangle ball = new Rectangle(245, 500, 10, 10);
+
+        Random randGen = new Random();
+
+        int time = 0;
+        int timerTick = 0;
+
+        int level1Bricks = 15;
+        int level2Bricks = 20;
+        int level3Bricks = 25;
+
+        int playerSpeed = 5;
+        float ballXSpeed = 6;
+        float ballYSpeed = -7;
+
+        int regBrickWidth = 30;
+        int regBrickHeight = 20;
+        int borderWidth = 10;
+
+        string gameState = "waiting";
+
+        bool leftDown = false;
+        bool rightDown = false;
+
+        List<Rectangle> brick = new List<Rectangle>();
+        List<int> brickWidth = new List<int>();
+        List<int> brickHeight = new List<int>();
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+        Pen blackPen = new Pen(Color.Black, 20);
+        Pen greenPen = new Pen(Color.DarkGreen, 20);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize(string level)
+        {
+            brick.Clear();
+            brickWidth.Clear();
+            brickHeight.Clear();
+
+            player.X = 220;
+            player.Y = 550;
+
+            ball.Y = 250;
+            ball.X = 245;
+
+            time = 0;
+            timerTick = 0;
+
+            titleLabel.Text = "3";
+            subTitleLabel.Text = "";
+
+            gameState = $"{level}";
+            gameTimer.Enabled = true;
+
+            if (gameState == "level 1")
+            {
+                drawBricks(level1Bricks, "level 1");
+            }
+            else if (gameState == "level 2")
+            {
+                drawBricks(level2Bricks, "level 2");
+            }
+            else if (gameState == "level 3")
+            {
+                drawBricks(level3Bricks, "level 3");
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == "waiting")
+            {
+                titleLabel.Text = "Brick Breaker";
+                subTitleLabel.Text = "Press space to begin\n\nor escape to exit";
+                timeLabel.Text = "";
+
+
+                e.Graphics.DrawRectangle(blackPen, border);
+                e.Graphics.FillEllipse(whiteBrush, ball);
+                e.Graphics.FillRectangle(blackBrush, player);
+            }
+            if (gameState == "level 1")
+            {
+                border.Height = 610;
+
+                timeLabel.Text = $"Time:{time}";
+
+                e.Graphics.DrawRectangle(greenPen, border);
+                e.Graphics.FillEllipse(whiteBrush, ball);
+                e.Graphics.FillRectangle(blackBrush, player);
+
+                for (int i = 0; i < brick.Count; i++)
+                {
+                    e.Graphics.FillRectangle(redBrush, brick[i]);
+                }
+            }
+            if (gameState == "level 2")
+            {
+                border.Height = 610;
+
+                timeLabel.Text = $"Time:{time}";
+
+                e.Graphics.DrawRectangle(greenPen, border);
+                e.Graphics.FillEllipse(whiteBrush, ball);
+                e.Graphics.FillRectangle(blackBrush, player);
+
+                for (int i = 0; i < brick.Count; i++)
+                {
+                    e.Graphics.FillRectangle(orangeBrush, brick[i]);
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Space:
+                    if (gameState == "waiting")
+                    {
+                        GameInitialize("level 1");
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        Application.Exit();
+                    }
+                    else if (gameState == "level 1" || gameState == "level 2" || gameState == "level 3")
+                    {
+
+                        titleLabel.Text = "";
+
+                        gameTimer.Enabled = false;
+                        subTitleLabel.Text = "Do you want to exit the game? \n\nAll current progress will be lost";
+
+                        continueButton.Enabled = true;
+                        closeButton.Enabled = true;
+
+                        continueButton.Visible = true;
+                        closeButton.Visible = true;
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            timerTick++;
+
+            if (timerTick == 50)
+            {
+                titleLabel.Text = "2";
+            }
+            else if (timerTick == 100)
+            {
+                titleLabel.Text = "1";
+            }
+            else if (timerTick == 150)
+            {
+                titleLabel.Text = "";
+            }
+
+            if (timerTick > 150)
+            {
+                ball.X += (int)ballXSpeed;
+                ball.Y += (int)ballYSpeed;
+            }
+            //move hero 
+            if (leftDown == true && player.X > 0 + borderWidth)
+            {
+                player.X -= playerSpeed;
+            }
+
+            if (rightDown == true && player.X < this.Width - player.Width - borderWidth)
+            {
+                player.X += playerSpeed;
+            }
+            if (ball.X < 0 + borderWidth || ball.X > this.Width - borderWidth - ball.Width)
+            {
+                ballXSpeed *= -1;
+            }
+            if (ball.Y < 0 + borderWidth)
+            {
+                ballYSpeed *= -1;
+            }
+            if (ball.Y > this.Height)
+            {
+                if (heart1.BackgroundImage != null)
+                {
+                    heart1.BackgroundImage = null;
+
+                    ball.X = 245;
+                    ball.Y = 200;
+
+                    Refresh();
+                    Thread.Sleep(1000);
+                }
+                else if (heart2.BackgroundImage != null)
+                {
+                    heart2.BackgroundImage = null;
+
+                    ball.X = 245;
+                    ball.Y = 200;
+
+                    Refresh();
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    heart3.BackgroundImage = null;
+
+                    Refresh();
+                    Thread.Sleep(200);
+
+                    gameState = "over";
+                    gameTimer.Stop();
+                }
+            }
+            // Set up player intersections
+            if (player.IntersectsWith(ball))
+            {
+                ballYSpeed *= -1;
+
+                Rectangle sec1 = new Rectangle(player.X, player.Y, 12, 10);
+                Rectangle sec2 = new Rectangle(player.X + 12, player.Y, 12, 10);
+                Rectangle sec3 = new Rectangle(player.X + 36, player.Y, 12, 10);
+                Rectangle sec4 = new Rectangle(player.X + 48, player.Y, 12, 10);
+                Rectangle sec5 = new Rectangle(player.X + 24, player.Y, 12, 10);
+
+                if (sec1.IntersectsWith(ball))
+                {
+                    if (ballXSpeed < 5)
+                    {
+                        ballXSpeed = -5;
+
+                        ball.Y = player.Y - ball.Height - 3;
+                    }
+                    else
+                    {
+                        ballXSpeed = -2;
+                        ball.Y = player.Y - ball.Height - 3;
+                    }
+                }
+                else if (sec2.IntersectsWith(ball))
+                {
+                    ballXSpeed *= 1.5f;
+
+                    ball.Y = player.Y - ball.Height - 3;
+                }
+                else if (sec3.IntersectsWith(ball))
+                {
+                    ballXSpeed *= 1.5f;
+
+                    ball.Y = player.Y - ball.Height - 3;
+                }
+                if (sec4.IntersectsWith(ball))
+                {
+                    if (ballXSpeed < 5)
+                    {
+                        ballXSpeed = 4;
+
+                        ball.Y = player.Y - ball.Height - 3;
+                    }
+                    else
+                    {
+                        ballXSpeed = 2;
+
+                        ball.Y = player.Y - ball.Height - 3;
+                    }
+                }
+                else if (sec5.IntersectsWith(ball))
+                {
+                    ball.Y = player.Y - ball.Height - 3;
+                }
+
+                if (ballXSpeed > 8)
+                {
+                    ballXSpeed *= 0.8f;
+                }
+                if (ballYSpeed > 8)
+                {
+                    ballYSpeed *= 0.8f;
+                }
+            }
+                for(int i = 0; i < brick.Count; i++)
+                {
+                    if (brick[i].IntersectsWith(ball))
+                    {
+                        brick.RemoveAt(i);
+
+                      if((ballXSpeed > 0) && (ball.Y > brick[i].Y && ball.Y < brick[i].Y + brick[i].Height))
+                        {
+                            ballXSpeed *= -1;
+                        }
+                      else if ((ballXSpeed < 0) && (ball.Y > brick[i].Y && ball.Y < brick[i].Y + brick[i].Height))
+                        {
+                            ballXSpeed *= -1;
+                        }
+                        else
+                        {
+                            ballYSpeed *= -1;
+                        }
+                    
+                }
+            }
+            Refresh();
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            gameTimer.Enabled = true;
+
+            continueButton.Enabled = false;
+            closeButton.Enabled = false;
+
+            continueButton.Visible = false;
+            closeButton.Visible = false;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void drawBricks(int levelBrick, string level)
+        {
+            int brickX = randGen.Next(0 + borderWidth, this.Width - borderWidth - regBrickWidth);
+            int brickY = randGen.Next(0 + borderWidth, 300);
+
+            Rectangle brick1 = new Rectangle(brickX, brickY, regBrickWidth, regBrickHeight);
+            brick.Add(brick1);
+
+            for (int i = 1; i < levelBrick; i++)
+            {
+                brickX = randGen.Next(0 + borderWidth, this.Width - borderWidth - regBrickWidth);
+                brickY = randGen.Next(0 + borderWidth, 200);
+
+                if (level == "level 1")
+                {
+                    Rectangle newBrick = new Rectangle(brickX, brickY, regBrickWidth, regBrickHeight);
+
+                    for (int a = 0; a < brick.Count; a++)
+                    {
+                        while (newBrick.IntersectsWith(brick[a])){
+                            brickX = randGen.Next(0 + borderWidth, this.Width - borderWidth - regBrickWidth);
+                            brickY = randGen.Next(0 + borderWidth, 200);
+
+                            newBrick = new Rectangle(brickX, brickY, regBrickWidth, regBrickHeight);
+                        } 
+                    }
+                   brick.Add(newBrick);
+                }
+                else
+                {
+                    int width = randGen.Next(regBrickWidth - 10, regBrickWidth + 5);
+                    int height = randGen.Next(regBrickHeight - 5, regBrickHeight + 10);
+
+                    brick.Add(new Rectangle(brickX, brickY, width, height));
+                }
+            }
+        }
+    }
+}
